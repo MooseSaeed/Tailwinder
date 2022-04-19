00367f874e137ce66f23d2128d05db4a6780cf28
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-04-19 12:51:11 +0200

    Tailwind + getting started with Homepage

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <template>
-  <HelloWorld />
+  <Homepage />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Homepage from "./components/main/Homepage.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Homepage,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/main/Homepage.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="flex justify-between items-center h-screen mx-5">
+    <header>
+      <h1 class="text-white font-bold text-3xl leading-relaxed">
+        An open source tool for creative tailwinders.
+      </h1>
+      <p>Create the best button design patterns for your website.</p>
+    </header>
+    <div class="bg-red-500 w-72 h-72"></div>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background-color: #00575e;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1200 800'%3E%3Cdefs%3E%3CradialGradient id='a' cx='0' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23093258'/%3E%3Cstop offset='1' stop-color='%23093258' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='b' cx='1200' cy='800' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%230a1556'/%3E%3Cstop offset='1' stop-color='%230a1556' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='c' cx='600' cy='0' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2323134f'/%3E%3Cstop offset='1' stop-color='%2323134f' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='d' cx='600' cy='800' r='600' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2300575E'/%3E%3Cstop offset='1' stop-color='%2300575E' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='e' cx='0' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%23121952'/%3E%3Cstop offset='1' stop-color='%23121952' stop-opacity='0'/%3E%3C/radialGradient%3E%3CradialGradient id='f' cx='1200' cy='0' r='800' gradientUnits='userSpaceOnUse'%3E%3Cstop offset='0' stop-color='%2338144D'/%3E%3Cstop offset='1' stop-color='%2338144D' stop-opacity='0'/%3E%3C/radialGradient%3E%3C/defs%3E%3Crect fill='url(%23a)' width='1200' height='800'/%3E%3Crect fill='url(%23b)' width='1200' height='800'/%3E%3Crect fill='url(%23c)' width='1200' height='800'/%3E%3Crect fill='url(%23d)' width='1200' height='800'/%3E%3Crect fill='url(%23e)' width='1200' height='800'/%3E%3Crect fill='url(%23f)' width='1200' height='800'/%3E%3C/svg%3E");
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>

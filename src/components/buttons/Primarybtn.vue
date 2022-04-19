babf43b81a66159f1115ab428eda3bd1f01ea78c
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-04-20 00:18:33 +0200

    Signup

--- src/components/buttons/Primarybtn.vue
+++ src/components/buttons/Primarybtn.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="relative group w-fit inline-block">
     <span
-      class="absolute inset-0 border-2 border-black border-dashed rounded-xl"
+      class="absolute inset-0 border-2 border-white border-dashed rounded-xl group-hover:translate-x-0.5 delay-150 duration-300 group-hover:translate-y-0.5"
     ></span>
 
     <div
-      class="relative flex items-end h-full transition-transform transform bg-white border-2 rounded-xl border-black group-hover:-translate-x-2 group-hover:-translate-y-2"
+      class="relative flex items-end h-full transition-transform transform border-2 rounded-xl border-white group-hover:-translate-x-1 group-hover:-translate-y-1 group-hover:bg-gradient-to-r from-green-400 to-blue-500"
     >
       <div class="py-2.5 px-7 transition-opacity group-hover:relative">
-        <h2 class="text-md font-semibold">
+        <h2 class="text-md font-semibold text-black group-hover:text-white">
           <slot />
         </h2>
       </div>
--- src/components/main/Homepage.vue
+++ src/components/main/Homepage.vue
@@ -21,15 +21,7 @@
             Tailwinder Tool
           </span>
         </button>
-        <button
-          class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-slate-400 to-blue-900 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-        >
-          <span
-            class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
-          >
-            Explore Community
-          </span>
-        </button>
+        <Primarybtn> Tailwinder Tool </Primarybtn>
       </div>
     </header>
     <div
@@ -39,8 +31,12 @@
 </template>
 
 <script>
+import Primarybtn from "../buttons/Primarybtn.vue";
 export default {
   name: "Homepage",
+  components: {
+    Primarybtn,
+  },
 };
 </script>
 

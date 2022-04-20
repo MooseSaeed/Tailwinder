9cb14e9fdcde15cff11539ab3e1d0544fdb4bab4
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-04-20 11:52:42 +0200

    working on btn

--- src/components/buttons/Primarybtn.vue
+++ src/components/buttons/Primarybtn.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="relative group w-fit inline-block">
     <span
-      class="absolute inset-0 border-2 border-white border-dashed rounded-xl group-hover:translate-x-0.5 delay-150 duration-300 group-hover:translate-y-0.5"
+      class="absolute inset-0 border-2 border-white border-dashed rounded-xl translate-x-0.5 group-hover:translate-x-0 delay-150 duration-300 translate-y-0.5 group-hover:translate-y-0"
     ></span>
 
     <div
-      class="relative flex items-end h-full transition-transform transform border-2 rounded-xl border-white group-hover:-translate-x-1 group-hover:-translate-y-1 group-hover:bg-gradient-to-r from-green-400 to-blue-500"
+      class="relative flex items-end h-full transition-all transform border-2 rounded-xl border-white -translate-x-1 group-hover:-translate-x-0 -translate-y-1 group-hover:-translate-y-0 bg-gradient-to-r from-green-400 to-blue-500 group-hover:bg-none"
     >
       <div class="py-2.5 px-7 transition-opacity group-hover:relative">
         <h2 class="text-md font-semibold text-black group-hover:text-white">

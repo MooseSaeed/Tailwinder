0cf1df9f0a3c0ad93ec95b2f9b93a34a9f659323
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-04-25 23:47:08 +0200

    showing buttons components dynamically

---
+++ src/components/Componentcards.vue
@@ -0,0 +1,48 @@
+<template>
+  <article
+    class="bg-blue-500 post-card text-white dark:text-black dark:hover:bg-blue-50 dark:hover:bg-none transition-colors py-6 px-5 flex flex-col justify-between duration-300 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"
+  >
+    <div>
+      <img
+        src="../assets/images/pic-sample-2.jpg"
+        alt="thumbnail"
+        class="rounded-xl"
+      />
+    </div>
+
+    <header class="mt-4">
+      <div class="mt-4">
+        <a href="#">
+          <h1 class="text-3xl font-bold">{{ buttonName }}</h1>
+        </a>
+
+        <div class="mt-4 text-gray-400 text-xs items-center">
+          <div>collection name: {{ collectionName }}</div>
+          <div>Button ID: {{ buttonId }}</div>
+          <div>Button Owner: {{ owner }}</div>
+          <div>Owner ID: {{ ownerId }}</div>
+        </div>
+      </div>
+    </header>
+
+    <div class="text-sm mt-8">
+      <p>some excerpt</p>
+    </div>
+
+    <footer
+      class="flex flex-col sm:flex-row lg:flex-col xl:flex-row justify-between items-center mt-8"
+    >
+      <div></div>
+
+      <a href="#" class="bg-gray-500">Read More </a>
+    </footer>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ["collectionName", "buttonId", "buttonName", "owner", "ownerId"],
+};
+</script>
+
+<style></style>
--- src/components/main/Community.vue
+++ src/components/main/Community.vue
@@ -1,11 +1,47 @@
 <template>
   <section class="mt-20 min-h-screen mx-10">
-    <h1 class="text-red-500 text-5xl">Hello community</h1>
+    <div class="lg:grid lg:grid-cols-3 gap-5">
+      <div v-for="document in documents" :key="document.$id">
+        <Componentcards
+          :collectionName="document.$collection"
+          :buttonId="document.$id"
+          :buttonName="document.buttonName"
+          :owner="document.owner"
+          :ownerId="document.ownerId"
+        />
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
-export default {};
+import Componentcards from "../Componentcards.vue";
+import { appwrite } from "../../utils";
+export default {
+  components: {
+    Componentcards,
+  },
+  data() {
+    return {
+      documents: null,
+    };
+  },
+  mounted() {
+    this.getAllDocuments();
+  },
+  methods: {
+    getAllDocuments() {
+      let promise = appwrite.database.listDocuments("Buttons");
+      let response = "";
+      promise.then((response) => {
+        this.documents = response.documents;
+        console.log(this.documents);
+      });
+
+      this.owner = response;
+    },
+  },
+};
 </script>
 
 <style></style>

7b3fd51bdb1003f724e8b22c57d7c1a2682e8468
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-04-27 18:32:30 +0200

    update for community

--- src/components/Componentcards.vue
+++ src/components/Componentcards.vue
@@ -11,30 +11,32 @@
     </div>
 
     <header class="mt-4 z-10">
+      <span
+        class="text-white text-xs font-semibold bg-blue-500 px-3 py-1 rounded-xl"
+        >{{ collectionName }}</span
+      >
       <div class="mt-4">
-        <a href="#">
+        <router-link :to="buttonUrl">
           <h1 class="text-3xl font-bold">{{ buttonName }}</h1>
-        </a>
-
-        <div class="mt-4 text-gray-400 text-xs items-center">
-          <div>collection name: {{ collectionName }}</div>
-        </div>
+        </router-link>
       </div>
     </header>
 
     <div class="text-sm mt-8 z-10">
-      <p>some excerpt</p>
+      <p>{{ description }}</p>
     </div>
 
     <footer
       class="z-10 flex flex-col sm:flex-row lg:flex-col xl:flex-row justify-between items-center mt-8"
     >
-      <div class="flex justify-center items-center gap-2">
-        <img
-          src="../assets/images/test.jpg"
-          class="rounded-full h-12 w-12 border-black border-2 transition-transform hover:scale-110 z-50"
-          alt=""
-        />
+      <div class="flex justify-center items-center gap-2 group">
+        <router-link :to="userAccUrl">
+          <img
+            src="../assets/images/test.jpg"
+            class="rounded-full h-12 w-12 border-black border-2 transition-transform group-hover:scale-110 z-50"
+            alt=""
+          />
+        </router-link>
         <router-link :to="userAccUrl">
           <h4 class="font-semibold">{{ owner }}</h4>
         </router-link>
@@ -51,7 +53,14 @@
 import Infobtn from "./buttons/Infobtn.vue";
 
 export default {
-  props: ["collectionName", "buttonId", "buttonName", "owner", "ownerId"],
+  props: [
+    "collectionName",
+    "buttonId",
+    "buttonName",
+    "owner",
+    "ownerId",
+    "description",
+  ],
   data() {
     return {
       userAccUrl: "/users/" + this.ownerId,
--- src/components/main/Community.vue
+++ src/components/main/Community.vue
@@ -1,5 +1,9 @@
 <template>
-  <section class="mt-20 min-h-screen mx-10">
+  <section class="min-h-screen mt-28 mx-10">
+    <div class="relative border border-gray-900/40 mb-10">
+      <span class="absolute text-2xl font-semibold -top-5 left-5">Buttons</span>
+    </div>
+
     <div class="lg:grid lg:grid-cols-3 gap-5">
       <div v-for="document in documents" :key="document.$id">
         <Componentcards
@@ -8,6 +12,7 @@
           :buttonName="document.buttonName"
           :owner="document.owner"
           :ownerId="document.ownerId"
+          :description="document.description"
         />
       </div>
     </div>
